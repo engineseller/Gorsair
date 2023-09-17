b9c4b71b9a616a7e2beecd5f5705b42b316562e7
Engine Seller <engineseller@users.noreply.github.com>
2023-09-17 02:24:35 +0200

    cmd/gorsair.go cannot indirect warnings (variable of type []string)

--- cmd/gorsair.go
+++ cmd/gorsair.go
@@ -67,8 +67,8 @@ func gorsair(cmd *cobra.Command, args []string) {
 
 	// results, err := scanner.Run()
 	results, warnings, err := scanner.Run()
-	if len(*warnings) > 0 {
-		log.Printf("run finished with warnings: %s\n", *warnings) // Warnings are non-critical errors from nmap.
+	if len(warnings) > 0 {
+		log.Printf("run finished with warnings: %s\n", warnings) // Warnings are non-critical errors from nmap.
 	}
 	if err != nil {
 		fmt.Println(err)
